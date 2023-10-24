0456c3bfe98a522d5cbbab0cbf050b9906a164df
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-10-24 12:33:45 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { fetchBreeds, fetchCatByBreed } from "/src/cat-api";
+
+fetchBreeds()
+// .then((breeds) => {
+//   const markup = breeds.map(({ id, name }) => `<option value="${id}">${name}</option>`);
+//   select.innerHTML = markup;
+// })
+
+// axios.defaults.headers.common["x-api-key"] = "live_V85TSSsGlPdM0g6OiuHucrzbUsTFhTuodM7JeCGIdl3NV5F5IMxg03OCh5PxmxeK";
+// console.log(axios.defaults.headers.common);
+
+// fetch()
