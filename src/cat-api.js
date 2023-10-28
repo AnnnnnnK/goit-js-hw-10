d84d03f936960ec315c3f40e5d7fd1c2664311ab
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-10-28 11:29:00 +0200

    init

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,22 +6,56 @@ const instance = axios.create({
 });
 
 
-
 function fetchBreeds() {
-    return instance.get('/breeds')
-    .then( res => res.data)
-    .catch(err => console.log(err))
-} 
-
-
-console.log(fetchBreeds());
-
-function fetchCatByBreed(breedId) {
-   return instance
-    .get(`/images/search?breed_ids=${breedId}`)
-    .then(response => response.data[0])
-    .catch((err) => console.log(err)
-    );
+  return instance.get('/breeds')
+  .then((resp) =>  resp.data);
 }
 
-export {fetchBreeds, fetchCatByBreed}
+function fetchCatByBreed(breedId) {
+  
+}
+
+export { fetchBreeds, fetchCatByBreed };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const instance = axios.create({
+//   baseURL: 'https://api.thecatapi.com/v1',
+//   headers: {'x-api-key': 'live_V85TSSsGlPdM0g6OiuHucrzbUsTFhTuodM7JeCGIdl3NV5F5IMxg03OCh5PxmxeK'}
+// });
+
+
+
+// function fetchBreeds() {
+//     return instance.get('/breeds')
+//     .then( res => res.data)
+//     .catch(err => console.log(err))
+// } 
+
+
+// console.log(fetchBreeds());
+
+// function fetchCatByBreed(breedId) {
+//    return instance
+//     .get(`/images/search?breed_ids=${breedId}`)
+//     .then(response => response.data[0])
+//     .catch((err) => console.log(err)
+//     );
+// }
+
+// export {fetchBreeds, fetchCatByBreed}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import { fetchBreeds, fetchCatByBreed } from "/src/cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js"
+
+const select = document.querySelector('.breed-select')
 
 fetchBreeds()
-// .then((breeds) => {
-//   const markup = breeds.map(({ id, name }) => `<option value="${id}">${name}</option>`);
-//   select.innerHTML = markup;
-// })
-
-// axios.defaults.headers.common["x-api-key"] = "live_V85TSSsGlPdM0g6OiuHucrzbUsTFhTuodM7JeCGIdl3NV5F5IMxg03OCh5PxmxeK";
-// console.log(axios.defaults.headers.common);
-
-// fetch()
+    .then(arr => {
+       const murkup = arr.map(({id, name}) => `<option value="${id}">${name}</option>`)
+        select.innerHTML = murkup;
+}).catch(err => console.log(err))
